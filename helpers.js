6f8180393e62fd8ab01fa60f9ff0fcf7c3be3b6e
Luca Guzzon <luca.guzzon@gmail.com>
2023-03-11 12:01:15 +0000

    🚑 fix: remove semicolons from helpers.js

--- helpers.js
+++ helpers.js
@@ -1,31 +1,31 @@
-import { execSync } from "child_process";
+import { execSync } from 'child_process'
 
 const getArgs = () => {
-  const args = process.argv.slice(2);
-  const result = {};
+  const args = process.argv.slice(2)
+  const result = {}
 
   for (let i = 0; i < args.length; i++) {
-    const arg = args[i];
-    const key = arg.replace(/^--/, '');
-    const nextArg = args[i + 1];
+    const arg = args[i]
+    const key = arg.replace(/^--/, '')
+    const nextArg = args[i + 1]
     if (/^--/.test(nextArg) || nextArg === undefined) {
-      result[key] = true;
+      result[key] = true
     } else {
-      result[key] = nextArg;
-      i++;
+      result[key] = nextArg
+      i++
     }
   }
 
-  return result;
-};
+  return result
+}
 
 const checkGitRepository = () => {
   try {
-    const output = execSync('git rev-parse --is-inside-work-tree', { encoding: 'utf-8' });
-    return output.trim() === 'true';
+    const output = execSync('git rev-parse --is-inside-work-tree', { encoding: 'utf-8' })
+    return output.trim() === 'true'
   } catch (err) {
-    return false;
+    return false
   }
-};
+}
 
 export { getArgs, checkGitRepository }
