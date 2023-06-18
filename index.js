aa0567998bc75d7bfa5a153aa63b211cbb9b0812
Luca Guzzon <luca.guzzon@gmail.com>
2023-06-18 18:25:46 +0200

    feat(index.js): 🚀 - add start function
    - Add `start` function to wrap `main` function
    - Call `start` function at the end of the file to execute the program

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 #!/usr/bin/env node
 
 'use strict'
+
 import { main } from './main.js'
 
-await main()
-process.exit(0)
+async function start () {
+  await main()
+  process.exit(0)
+}
+
+await start()
